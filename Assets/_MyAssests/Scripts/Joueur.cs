c2ba6570d02542ef35dbf160556eb2bcb993fa57
JoyceVanelle <tsatiamomo@gmail.com>
2023-04-25 18:37:34 -0400

    not done

--- Assets/_MyAssests/Scripts/Joueur.cs
+++ Assets/_MyAssests/Scripts/Joueur.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Joueur : MonoBehaviour
 {
+
+    [SerializeField] private float _vitesse = 100.0f;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        MouvementsJoueur();
+    }
+
+    private void MouvementsJoueur()
+    {
+        float horizInput = Input.GetAxis("Horizontal");
+        float vertiInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizInput, vertiInput, 0f);
+        transform.Translate(direction * Time.deltaTime * _vitesse);
+        // ajuster la position en y (vertical) en utilisant le math.clamp
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4f, 3.5f), 0f);
+        //  ajuster la position en x
+        if (transform.position.x < -9.5f)
+        {
+            transform.position = new Vector3(9.5f, transform.position.y, 0f);
+        }
+        else if (transform.position.x > 9.5f)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0f);
+        }
     }
 }
